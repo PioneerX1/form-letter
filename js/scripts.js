1cf5b02274b8ec50016cfbe187034771c16c8233
Diego Aguilar and Mick Sexton <diego@>
2017-05-31 17:24:11 -0700

    add field for the senders name

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    var myNameInput = $("input#my-name").val();
+    var firstNameInput = $("input#first-name").val();
+    var lastNameInput = $("input#last-name").val();
+    var streetInput = $("input#street-address").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+
+    $(".my-name").text(myNameInput);
+    $(".first-name").text(firstNameInput);
+    $(".last-name").text(lastNameInput);
+    $(".street-address").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+
+    event.preventDefault();
+  });
+});
